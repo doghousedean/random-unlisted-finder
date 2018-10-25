21b219f0857769951327edd0953e3f3f7395f7c9
Dean Moore <dean.moore@zeninternet.co.uk>
2018-10-25 15:40:27 +0100

    Added check for live

--- rand-unlisted.py
+++ rand-unlisted.py
@@ -7,7 +7,6 @@ import time
 from urllib.request import urlopen
 
 out_file = 'urllist.txt'
-<<<<<<< HEAD
 tries = 100000
 delay = 3
 
@@ -19,21 +18,26 @@ for x in range(tries):
     ran = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(11)])
     out = "https://youtu.be/{}".format(ran)
     found = 0
+    live = 0
     with urlopen(out) as html:
         for line in html:
             line = line.decode('utf-8')
-            if 'unlisted' in line:
+            if 'This video is unlisted' in line:
                 print("Found in try {} -  {}".format(x,out))
                 fh.write(out)
                 found = 1
                 break
-            if 'This video is unavailable' not in line:
-                print("Live video: {}".format(out))
-                fh.write("LIVE:{}".format(out))
+            if 'This video is unavailable' in line:
+                print("Video Unavailable: {}".format(out))
                 break
+        live = 1
 
     if found == 0:
         print("Nothing in try {} - {}".format(x,out))
+    if live = 1:
+        print("Live video: {}".format(out))
+        fh.write("LIVE:{}".format(out))
+
     time.sleep(delay)
 fh.close()
 
